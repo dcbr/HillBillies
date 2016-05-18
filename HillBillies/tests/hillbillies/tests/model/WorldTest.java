d88c8b4d7df14fe138b5047bae48459c6426084e
Bram <bram.decooman@gmail.com>
2016-05-19 00:20:27 +0200

    Created test classes

--- HillBillies/tests/hillbillies/tests/model/WorldTest.java
+++ HillBillies/tests/hillbillies/tests/model/WorldTest.java
@@ -46,4 +46,219 @@ public class WorldTest {
 		assertEquals(nbZ, world.getNbCubesZ());
 	}
 
+	@Test
+	public void isValidPosition() throws Exception {
+
+	}
+
+	@Test
+	public void getNbCubesX() throws Exception {
+
+	}
+
+	@Test
+	public void getNbCubesY() throws Exception {
+
+	}
+
+	@Test
+	public void getNbCubesZ() throws Exception {
+
+	}
+
+	@Test
+	public void getMinPosition() throws Exception {
+
+	}
+
+	@Test
+	public void getMaxPosition() throws Exception {
+
+	}
+
+	@Test
+	public void hasAsFaction() throws Exception {
+
+	}
+
+	@Test
+	public void canHaveAsFaction() throws Exception {
+
+	}
+
+	@Test
+	public void hasProperFactions() throws Exception {
+
+	}
+
+	@Test
+	public void getNbFactions() throws Exception {
+
+	}
+
+	@Test
+	public void addFaction() throws Exception {
+
+	}
+
+	@Test
+	public void getFactions() throws Exception {
+
+	}
+
+	@Test
+	public void spawnUnit() throws Exception {
+
+	}
+
+	@Test
+	public void addUnit() throws Exception {
+
+	}
+
+	@Test
+	public void hasAsUnit() throws Exception {
+
+	}
+
+	@Test
+	public void canHaveAsUnit() throws Exception {
+
+	}
+
+	@Test
+	public void hasProperUnits() throws Exception {
+
+	}
+
+	@Test
+	public void getNbUnits() throws Exception {
+
+	}
+
+	@Test
+	public void removeUnit() throws Exception {
+
+	}
+
+	@Test
+	public void getUnits() throws Exception {
+
+	}
+
+	@Test
+	public void getWorkshops() throws Exception {
+
+	}
+
+	@Test
+	public void isCubePassable() throws Exception {
+
+	}
+
+	@Test
+	public void getSpawnPosition() throws Exception {
+
+	}
+
+	@Test
+	public void getCube() throws Exception {
+
+	}
+
+	@Test
+	public void getDirectlyAdjacentCubes() throws Exception {
+
+	}
+
+	@Test
+	public void getNeighbouringCubes() throws Exception {
+
+	}
+
+	@Test
+	public void getDirectlyAdjacentCubesSatisfying() throws Exception {
+
+	}
+
+	@Test
+	public void getNeighbouringCubesSatisfying() throws Exception {
+
+	}
+
+	@Test
+	public void getDirectlyAdjacentCubesPositions() throws Exception {
+
+	}
+
+	@Test
+	public void getNeighbouringCubesPositions() throws Exception {
+
+	}
+
+	@Test
+	public void isAdjacentSolid() throws Exception {
+
+	}
+
+	@Test
+	public void isLowerSolid() throws Exception {
+
+	}
+
+	@Test
+	public void advanceTime() throws Exception {
+
+	}
+
+	@Test
+	public void getUnitsInCube() throws Exception {
+
+	}
+
+	@Test
+	public void hasAsMaterial() throws Exception {
+
+	}
+
+	@Test
+	public void canHaveAsMaterial() throws Exception {
+
+	}
+
+	@Test
+	public void hasProperMaterials() throws Exception {
+
+	}
+
+	@Test
+	public void getNbMaterials() throws Exception {
+
+	}
+
+	@Test
+	public void addMaterial() throws Exception {
+
+	}
+
+	@Test
+	public void removeMaterial() throws Exception {
+
+	}
+
+	@Test
+	public void getMaterials() throws Exception {
+
+	}
+
+	@Test
+	public void getLogs() throws Exception {
+
+	}
+
+	@Test
+	public void getBoulders() throws Exception {
+
+	}
+
 }
--- HillBillies/tests/hillbillies/tests/programs/TaskFactoryTest.java
+++ HillBillies/tests/hillbillies/tests/programs/TaskFactoryTest.java
@@ -7,18 +7,203 @@ import hillbillies.part3.programs.expressions.HerePosition;
 import hillbillies.part3.programs.expressions.True;
 import hillbillies.part3.programs.statements.Print;
 import hillbillies.part3.programs.statements.While;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Created by Bram on 27-4-2016.
+ * Test class for the TaskFactory class
+ * @author Kenneth & Bram
+ * @version 1.0
  */
 public class TaskFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void createTasks() throws Exception {
+
+    }
+
+    @Test
+    public void createAssignment() throws Exception {
+
+    }
+
     @Test
     public void createWhile() throws Exception {
         TaskFactory f = new TaskFactory();
         f.createWhile(new HerePosition(),new Print(new True()), null);// Dit gaat wel, maar gaat classcastException throwen zodra het wordt uitgevoerd
         //new While(new HerePosition(),new Print(new True()));// Dit lukt niet
         new While(new False(),new Print(new False()));
+    }
+
+    @Test
+    public void createIf() throws Exception {
+
+    }
+
+    @Test
+    public void createBreak() throws Exception {
+
+    }
+
+    @Test
+    public void createPrint() throws Exception {
+
+    }
+
+    @Test
+    public void createSequence() throws Exception {
+
+    }
+
+    @Test
+    public void createMoveTo() throws Exception {
+
+    }
+
+    @Test
+    public void createWork() throws Exception {
+
+    }
+
+    @Test
+    public void createFollow() throws Exception {
+
+    }
+
+    @Test
+    public void createAttack() throws Exception {
+
+    }
+
+    @Test
+    public void createReadVariable() throws Exception {
+
+    }
+
+    @Test
+    public void createIsSolid() throws Exception {
+
+    }
+
+    @Test
+    public void createIsPassable() throws Exception {
+
+    }
+
+    @Test
+    public void createIsFriend() throws Exception {
+
+    }
+
+    @Test
+    public void createIsEnemy() throws Exception {
+
+    }
+
+    @Test
+    public void createIsAlive() throws Exception {
+
+    }
+
+    @Test
+    public void createCarriesItem() throws Exception {
+
+    }
+
+    @Test
+    public void createNot() throws Exception {
+
+    }
+
+    @Test
+    public void createAnd() throws Exception {
+
+    }
+
+    @Test
+    public void createOr() throws Exception {
+
+    }
+
+    @Test
+    public void createHerePosition() throws Exception {
+
+    }
+
+    @Test
+    public void createLogPosition() throws Exception {
+
+    }
+
+    @Test
+    public void createBoulderPosition() throws Exception {
+
+    }
+
+    @Test
+    public void createWorkshopPosition() throws Exception {
+
+    }
+
+    @Test
+    public void createSelectedPosition() throws Exception {
+
+    }
+
+    @Test
+    public void createNextToPosition() throws Exception {
+
+    }
+
+    @Test
+    public void createPositionOf() throws Exception {
+
+    }
+
+    @Test
+    public void createLiteralPosition() throws Exception {
+
+    }
+
+    @Test
+    public void createThis() throws Exception {
+
+    }
+
+    @Test
+    public void createFriend() throws Exception {
+
+    }
+
+    @Test
+    public void createEnemy() throws Exception {
+
+    }
+
+    @Test
+    public void createAny() throws Exception {
+
+    }
+
+    @Test
+    public void createTrue() throws Exception {
+
+    }
+
+    @Test
+    public void createFalse() throws Exception {
+
     }
 
 }
